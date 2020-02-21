58affc4686d77836249b2773d7a7236b51c11dd4
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-02-21 19:51:40 +0200

    add first activity

---
+++ app/src/main/java/dmytro/kuchura/skyupschedule/AirportSelectActivity.java
@@ -0,0 +1,14 @@
+package dmytro.kuchura.skyupschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AirportSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airport_select);
+    }
+}
--- app/src/main/java/dmytro/kuchura/skyupschedule/MainActivity.java
+++ app/src/main/java/dmytro/kuchura/skyupschedule/MainActivity.java
@@ -1,14 +1,30 @@
 package dmytro.kuchura.skyupschedule;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    int SPLASH_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mySuperIntent = new Intent(MainActivity.this, AirportSelectActivity.class);
+                startActivity(mySuperIntent);
+
+                finish();
+
+            }
+        }, SPLASH_TIME);
     }
 }
