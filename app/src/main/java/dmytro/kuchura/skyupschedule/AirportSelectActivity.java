b7993fa935ac0e42ed874a51b69f9db1a21378ea
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-02-22 22:02:49 +0200

    add airport activity

--- app/src/main/java/dmytro/kuchura/skyupschedule/AirportSelectActivity.java
+++ app/src/main/java/dmytro/kuchura/skyupschedule/AirportSelectActivity.java
@@ -2,7 +2,9 @@ package dmytro.kuchura.skyupschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AirportSelectActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class AirportSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airport_select);
+    }
+
+    public void setLvivAirport(View view) {
+        Intent mySuperIntent = new Intent(AirportSelectActivity.this, AirportActivity.class);
+        startActivity(mySuperIntent);
+
+        finish();
     }
 }
